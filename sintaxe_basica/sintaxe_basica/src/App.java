d8ca9c62c40aa10dc34d1421f64df8daabbb840e
FrancimarAlexandre <francimaralexandredt@gmail.com>
2026-04-07 08:56:35 -0300

    variáveis

---
+++ sintaxe_basica/sintaxe_basica/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("Variáveis");
+        int n = 10;
+        String nome = "Fulano";
+        Boolean boo = false;
+        double valor = 1.99;
+        System.out.println("Inteiros:" + n + "\n" + "String: " + nome + "\n" + "Boolean:" + boo + "\n" + "double:" + valor);
+    }
+}
